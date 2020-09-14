8573cd927ed3c6ffca9556fbfd28e01377d1b64a
Adir Burke on Laptop <adir@hanave.com>
2020-09-15 09:36:13 +1000

    Updating

--- Package.swift
+++ Package.swift
@@ -27,9 +27,13 @@ let package = Package(
         .library(name: "PerfectNet", targets: ["PerfectNet"])
     ],
     dependencies: [
-        .package(url: "https://github.com/PerfectlySoft/Perfect-Crypto.git", from: "4.0.0"),
+        // .package(url: "https://github.com/PerfectlySoft/Perfect-Crypto.git", from: "4.0.0"),
         .package(url: "https://github.com/PerfectlySoft/Perfect-LinuxBridge.git", from: "3.0.0"),
-        .package(url: "https://github.com/PerfectlySoft/Perfect-Thread.git", from: "3.0.0")
+        // .package(url: "https://github.com/PerfectlySoft/Perfect-Thread.git", from: "3.0.0")
+                // .package(url: "https://github.com/PerfectlySoft/Perfect-Crypto.git", from: "4.0.0"),
+        .package(path: "~/Dev/Perfect/Perfect-Crypto"),
+        // .package(url: "https://github.com/PerfectlySoft/Perfect-Thread.git", from: "3.0.0")
+        .package(path: "~/Dev/Perfect/Perfect-Thread"),
     ],
     targets: [
         .target(name: "PerfectNet", dependencies: ["PerfectCrypto", "LinuxBridge", "PerfectThread"]),
@@ -46,8 +50,10 @@ let package = Package(
         .library(name: "PerfectNet", targets: ["PerfectNet"])
     ],
     dependencies: [
-        .package(url: "https://github.com/PerfectlySoft/Perfect-Crypto.git", from: "4.0.0"),
-        .package(url: "https://github.com/PerfectlySoft/Perfect-Thread.git", from: "3.0.0")
+        // .package(url: "https://github.com/PerfectlySoft/Perfect-Crypto.git", from: "4.0.0"),
+        .package(path: "~/Dev/Perfect/Perfect-Crypto"),
+        // .package(url: "https://github.com/PerfectlySoft/Perfect-Thread.git", from: "3.0.0")
+        .package(path: "~/Dev/Perfect/Perfect-Thread"),
     ],
     targets: [
         .target(name: "PerfectNet", dependencies: ["PerfectCrypto", "PerfectThread"]),
